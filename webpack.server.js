7594a28505efbd235595270de0d77826cccb8f5d
dipu-725462 <dipu.saha@practo.com>
2021-06-27 20:44:00 +0530

    feat: Added SSR wireframe
    1.Should build the bundle to be served
    
    2.An then running the server to serve the bundle

---
+++ webpack.server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const nodeExternals = require('webpack-node-externals');
+
+
+module.exports = {
+  mode: 'production',
+  externals: [nodeExternals()],
+  target: 'node',
+  entry: './src/index',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+     {
+      test: /\.js$/,
+      loader: "babel-loader",
+      exclude: path.resolve(__dirname, "node_modules")
+     }
+    ],
+   },
+  resolve: {
+    extensions: ['.jsx', '.js'],
+  }
+}
